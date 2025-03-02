4071e4ec261d7ae981b1b9de8fed4d8afd99e590
nikhiln22 <nikhilnhegde@gmail.com>
2025-03-03 00:06:14 +0530

    feat:added persisting functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store, { persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import habitsReducer from './habit-slice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
-import { configureStore } from "@reduxjs/toolkit"
-import habitsReducer from './habit-slice'
+
+const persistConfig = {
+    key: 'habits',
+    storage,
+    whitelist: ['habits'],
+};
+
+const persistedReducer = persistReducer(persistConfig, habitsReducer);
 
 const store = configureStore({
     reducer: {
-        habits: habitsReducer
+        habits: persistedReducer,
     },
-})
 
-export type RootState = ReturnType<typeof store.getState>
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export default store
+export default store;
